5f01d903628c360c80ce9106693378c6b14d45cd
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-06-29 14:59:33 +0900

    nest test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -12,7 +13,39 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', () => { //테스트 실행 형식
     expect(service).toBeDefined();
+  });
+
+  it("should be 4", () => {
+    expect(2+3).toEqual(5)
+  })
+
+  describe("getAll", () => {
+    it("should return array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+    it("should return a movie", () => {
+      service.create({
+        title: "test MOvie",
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e.message).toEqual('movie with id 999 not found');
+      }
+    })
   });
 });
